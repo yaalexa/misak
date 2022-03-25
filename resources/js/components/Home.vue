dd4f3f5285b1d498fc6337ace19c3039534c91dc
yaalexa <yaalexa25@gmail.com>
2022-03-25 10:01:35 -0500

    subiendo repositorio
    primer commit misak

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="container mt-4">
+        <div class="row">
+            <div class="col text-center">
+                <h1>MATERIAL</h1>
+            </div>
+        </div>
+    </div>
+</template>
