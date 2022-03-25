7a8975adf44ad04e1e7bc89936e7bdab75f45aa9
yaalexa <yaalexa25@gmail.com>
2022-03-25 11:34:17 -0500

    componente autores
    componente vue authores

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 //de esta forma nos genera todas las rutas
 Route::resource('blog',App\Http\Controllers\BlogController::class);
-Route::resource('autor',App\Http\Controllers\AuthorController::class);
-/*Route::get('/author',[AuthorController::class,'index']);// muestra todos los registros
+Route::resource('author',App\Http\Controllers\AuthorController::class);
+Route::get('/author',[AuthorController::class,'index']);// muestra todos los registros
 Route::post('/author',[AuthorController::class,'store']);//crea un registro
 Route::put('/author/{id}',[AuthorController::class,'update']);//actualiza un registro
-Route::delete('/author/{id}',[AuthorController::class,'destroy']);//elimina un registro*/
+Route::delete('/author/{id}',[AuthorController::class,'destroy']);//elimina un registro
